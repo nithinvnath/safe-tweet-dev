a7559383ceb445c91245162c1c006af9d3d2e084
Nithin V Nath <vnath.nithin@gmail.com>
2013-03-12 03:23:39 +0530

    Merged twitter_api.py with indexing.py

--- indexing.py
+++ indexing.py
@@ -3,9 +3,65 @@
 """ Reads tweets from a file and removes the stopwords
 Assumes the tweets are of the form <username>: <tweet>"""
 
+import tweepy
+from webbrowser import open as webopen
+
 import re
 from nltk.corpus import stopwords, wordnet
 from nltk.stem import WordNetLemmatizer
+from nltk.stem import PorterStemmer
+
+#---------------------------------------------------------------------#
+#							TWITTER API								  #
+#---------------------------------------------------------------------#
+
+# #Consumer token and secret are specific to the application
+# #Can  be found in dev.twitter.com/apps
+# consumer_token = "pDeVZbNzK6HXocUuhwLqBg"
+# consumer_secret = "Itj45FWSmMr0VmrNPJuO2KIaIt3hzayY2ywVteh2M"
+
+# #To get authorization from user using OAuth
+# auth = tweepy.OAuthHandler(consumer_token,consumer_secret)
+# auth_url = auth.get_authorization_url(signin_with_twitter=True)
+
+# # print "Authorize: " + auth_url
+# webopen(auth_url)
+# verifier = raw_input('PIN: ').strip()
+# auth.get_access_token(verifier)
+
+# access_token = auth.access_token.key
+# access_secret = auth.access_token.secret
+# # access_token = "45142783-KrrjDjWf0M3OKuq1Ckb9Bi6WpNX9ZQnhurPM1wxDA"
+# # access_secret = "b6a6OHD39u1GNJMfkTp5LGRWCk7WWgTZgPkE6sgp8wo"
+
+# #Access the user's account using the OAuth credentials
+# auth.set_access_token(access_token,access_secret)
+
+# api = tweepy.API(auth)	#authenticated api module
+
+# if api.me().name is not None:
+# 	print "Successfully authenticated"
+# else:
+# 	print "Authentication failed!"
+
+# pub = api.home_timeline()
+
+#--------------------------------------
+
+consumer_token = "pDeVZbNzK6HXocUuhwLqBg"
+consumer_secret = "Itj45FWSmMr0VmrNPJuO2KIaIt3hzayY2ywVteh2M"
+auth = tweepy.OAuthHandler(consumer_token,consumer_secret)
+auth_url = auth.get_authorization_url(signin_with_twitter=True)
+access_token = "45142783-KrrjDjWf0M3OKuq1Ckb9Bi6WpNX9ZQnhurPM1wxDA"
+access_secret = "b6a6OHD39u1GNJMfkTp5LGRWCk7WWgTZgPkE6sgp8wo"
+auth.set_access_token(access_token,access_secret)
+api = tweepy.API(auth)
+pub = api.home_timeline()
+
+#---------------------------------------------------------------------#
+#							PREPROCESSING							  #
+#---------------------------------------------------------------------#
+
 
 wnl = WordNetLemmatizer()
 #---------------------------------------------------------------------#
@@ -19,22 +75,55 @@ def removeStopwords (line):
 			important_words.append(word)
 	return important_words
 
+def stemming(line):
+	stemmer = PorterStemmer()
+	line_array = line.split(" ")
+	for word in line_array:
+		replace_word = stemmer.stem(word)
+		#print replace_word	
+		line = line.replace(word,replace_word)
+	return line
+
+
+#---------------------------------------------------------------------#
+#							INDEXING								  #
+#---------------------------------------------------------------------#
+
 #---------------------------------------------------------------------#
 #Twitter converts all urls to http://t.co/<something>
-#this regular expression checks for all urls in a ling
-#and return them as a list
+#Checks for presence of URL using API. Takes as input a tweet object
 
-def hasUrl (line):
-	u=re.compile('http\:\/\/t\.co\/[a-zA-Z0-9]+')
-	return u.findall(line)
+
+def hasUrl (tweet):
+	#u=re.compile('http\:\/\/t\.co\/[a-zA-Z0-9]+')
+	#return u.findall(line)
+	if tweet.entities['urls'] == []:
+		return 0
+	else:
+		return 1
+
+
 #---------------------------------------------------------------------#
 #function to check the presence of a username in the 
 #tweet
-#EDIT: Can be taken directly from the tweet obtained. A field exists
 
-def hasUsernames(line):
-	u=re.compile('\@[a-zA-Z0-9\_]+')
-	return u.findall(line)
+
+def hasUsernames(tweet):
+	#u=re.compile('\@[a-zA-Z0-9\_]+')
+	#return u.findall(line)
+	if tweet.entities['user_mentions'] == []:
+		return 0
+	else:
+		return 1
+
+#---------------------------------------------------------------------#
+#Function to check if the author of tweet is verified user or not
+
+def verifiedUser(user):
+	if user.verified is True:
+		return 1
+	else:
+		return 0
 
 
 #---------------------------------------------------------------------#
@@ -54,6 +143,7 @@ def emphExist(line):
 	return presence
 
 
+
 #replaces the word after removing repeated letters
 def emphReplace(word):
 	emp_regexp = re.compile(r'(\w*)(\w)\2(\w*)')
@@ -67,21 +157,49 @@ def emphReplace(word):
 
 #---------------------------------------------------------------------#
 #checks if the tweet is a retweet
-#takes as input the raw tweet
+#takes as input tweet object
 def isRetweet(tweet):
-	p = re.compile('([A-Za-z\ ]+\:)(\\t)(.*)')
-	t = m.group(3)
-	if (t[0]=='R' and t[1]=='T' and t[2]==' '):
+	# p = re.compile('([A-Za-z\ ]+\:)(\\t)(.*)')
+	# m = p.match(tweet)
+	# t = m.group(3)
+	# if (t[0]=='R' and t[1]=='T' and t[2]==' '):
+	# 	return 1
+	# else:
+	# 	return 0
+	if hasattr(tweet,'retweeted_status'):
 		return 1
 	else:
 		return 0
+
 
 #---------------------------------------------------------------------#
 #checks for presence of profanity. Compares each word from file 'bad-words.txt'
 #expects a list of words
 def hasProfanity(words):
 	count = 0
-	f = open('./Corpus/bad-words.txt','r')
+	f = open('./Corpus/profanity.txt','r')
+	lis = []
+	for badword in f:
+		lis.append(badword.strip())
+	for badword in lis:
+		if badword in words:
+			count = count + 1
+	return count
+
+def hasExplicit(words):
+	count = 0
+	f = open('./Corpus/explicit.txt','r')
+	lis = []
+	for badword in f:
+		lis.append(badword.strip())
+	for badword in lis:
+		if badword in words:
+			count = count + 1
+	return count
+
+def hasHateSpeech(words):
+	count = 0
+	f = open('./Corpus/hate-speech.txt','r')
 	lis = []
 	for badword in f:
 		lis.append(badword.strip())
@@ -93,13 +211,43 @@ def hasProfanity(words):
 #---------------------------------------------------------------------#
 #The following regular expression checks for three groups
 #name followed by a colon, a tab and finally the tweet
-p = re.compile('([a-z\ ]+\:)(\\t)(.*)',re.IGNORECASE)
-f = open('test','rb')
-for tweet in f:
-	m = p.match(tweet)
-	if m is not None:		#the file may contain invalid contents
-		print tweet
-		print hasUsernames(tweet)
-		#print removeStopwords(m.group(3))
-f.close()
+# p = re.compile('([a-z\ ]+\:)(\\t)(.*)',re.IGNORECASE)
+# f = open('test','rb')
+# for tweet in f:
+# 	m = p.match(tweet)
+# 	if m is not None:		#the file may contain invalid contents
+# 		print tweet
+# 		print hasUsernames(tweet)
+# 		#print removeStopwords(m.group(3))
+# f.close()
 
+#Feature Vector: [hasUrl, verifiedUser, hasUsername,emphExist,isRetweet,Profanity,Explicit,HateSpeech]
+
+i=0
+
+#for tweet in pub:
+	# fvector[i][0] = hasUrl(tweet)
+	# fvector[i][1] = verifiedUser(tweet)
+	# fvector[i][2] = hasUsernames(tweet)
+	# tweet_text = tweet.text
+	# fvector[i][3] = emphExist(tweet_text)
+	# fvector[i][4] = isRetweet(tweet_text)
+	# tweet_words = removeStopwords(tweet_text)
+	# fvector[i][5] = hasProfanity(tweet_words)
+	# fvector[i][6] = hasExplicit(tweet_words)
+	# fvector[i][7] = hasHateSpeech(tweet_words)
+	# print fvector[i]
+tweet = pub[1]
+print tweet.text
+print "URL: ", hasUrl(tweet)
+print "User: ", verifiedUser(tweet.user)
+print "Mentions: ", hasUsernames(tweet)
+tweet_text = tweet.text
+print "Emphasis: ", emphExist(tweet_text)
+print "Retweet: ", isRetweet(tweet)
+tweet_words = removeStopwords(tweet_text)
+print "Profanity: ", hasProfanity(tweet_words)
+print "Explicit: ", hasExplicit(tweet_words)
+print "HateSpeech: ", hasHateSpeech(tweet_words)
+#print fvector[i]
+print "******************************************"
